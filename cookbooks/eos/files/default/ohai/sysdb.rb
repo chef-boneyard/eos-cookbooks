b3ed6bfcf6b6d290fb5fb10cb58c6410c9f4aa31
jamesc <james@opscode.com>
2012-09-17 23:53:36 -0700

    sysdb ohai plugin.  Hooked up to interfaces

---
+++ cookbooks/eos/files/default/ohai/sysdb.rb
@@ -0,0 +1,19 @@
+#
+# Ohai plugin to grab info from Sysdb on an Arista switch
+# running EOS
+#
+
+require "yajl"
+
+provides "sysdb"
+
+sysdb = Mash.new
+
+# Interfaces
+popen4("/mnt/flash/scripts/interface.py --get") do |pid, stdin, stdout, stderr|
+  stdin.close
+  parser = Yajl::Parser.new
+  sysdb['interface'] = parser.parse(stdout.read)
+end
+
+sysdb sysdb
