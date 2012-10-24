e59d8debe4db6031714f21f70a53cc2530db2f6b
jamesc <james@opscode.com>
2012-10-23 21:16:20 -0700

    bump eos_demo version

--- cookbooks/eos_demo/metadata.rb
+++ cookbooks/eos_demo/metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "james@opscode.com"
 license          "Apache2"
 description      "Installs/Configures eos_demo"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 
 depends "eos"
