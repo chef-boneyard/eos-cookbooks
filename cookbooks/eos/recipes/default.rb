99461f0b900429c145dd3618553c2dd85f46f8e7
jamesc <james@opscode.com>
2012-10-23 19:51:24 -0700

    work around bug in remote_directory and whyrun causing first run to fail

--- cookbooks/eos/recipes/default.rb
+++ cookbooks/eos/recipes/default.rb
@@ -7,14 +7,17 @@
 
 # Install helper scripts for ohai and setting switch configuration
 
-remote_directory node['eos']['scripts'] do
+
+directory node['eos']['scripts'] do
   owner 'root'
   group 'eosadmin'
   mode '0775'
-  files_owner 'root'
-  files_group 'eosadmin'
-  files_mode '0775'
   recursive false
-  purge true
 end
 
+template "#{node['eos']['scripts']}/interface.py" do
+  action :create
+  owner 'root'
+  group 'eosadmin'
+  mode '0755'
+end
